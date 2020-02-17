0e258be82bec945d84a1b2f87c35a53a2cdad3c2
Vishnu Prasad Satyanarayana <vishnuprasad.s84@gmail.com>
2020-02-17 17:06:25 -0500

    Solutions commited

--- src/main/java/com/example/req/RequirementsApplication.java
+++ src/main/java/com/example/req/RequirementsApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import com.example.req.linked.list.SortedLinkedList;
 import com.example.req.tree.SortedTree;
 
 @SpringBootApplication
@@ -14,6 +15,31 @@ public class RequirementsApplication {
 		SpringApplication.run(RequirementsApplication.class, args);
 	}
 
+	@Bean
+	CommandLineRunner runTask() {
+		return args -> {
+			System.out.println("Initializing the Sorted Linked List");
+			SortedLinkedList sortedList = new SortedLinkedList();
+			// Data set: 5, 7, 11, 20, 27, 36, 36, 54
+			sortedList.add(54);
+			sortedList.add(36);
+			sortedList.add(36);
+			sortedList.add(27);
+			sortedList.add(20);
+			sortedList.add(11);
+			sortedList.add(7);
+			sortedList.add(5);
+			System.out.println("Contents:" + sortedList);
+			// 34  42  29  33  4
+			sortedList.add(34);
+			sortedList.add(42);
+			sortedList.add(29);
+			sortedList.add(33);
+			sortedList.add(4);
+			System.out.println("Contents:" + sortedList);
+		};
+	}
+	
 	@Bean
 	CommandLineRunner runTreeTask() {
 		return args -> {
